8fff83fed254cad50c35991eb53d96b58a8f1e9a
Siwat Sirichai <siwat@siwatinc.com>
2019-08-12 21:33:52 +0700

    Split Variant

---
+++ WaterishOS-mcp2.0/config.h
@@ -0,0 +1,11 @@
+const char ssid[] = "";
+const char password[] = "";
+const char* telemetryhost = "";
+const char* telemetrykey = "";
+const int telemetryport = 1883;
+const char* telemetryuser = "";
+const byte InteruptPinA=1;
+const byte InteruptPinB=3;
+const byte arduinoInterrupt=1;
+const byte buttonPin = 0;
+const String nodename="devkit";
