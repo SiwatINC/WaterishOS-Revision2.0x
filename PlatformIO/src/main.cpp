47d5a537259b269c15870ec2508d4a8b01b9c02f
Siwat Sirichai <siwat@siwatinc.com>
2020-01-08 03:45:29 +0000

    Update PlatformIO/src/main.cpp

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include <PubSubClient.h>
-#include <MqttWildcard.h>
-#include <PubSubClientTools.h>
 #include <BearSSLHelpers.h>
 #include <CertStoreBearSSL.h>
 #include <ESP8266WiFi.h>
@@ -31,10 +28,8 @@
 Adafruit_MCP23017 mcp;
 long tslr = 0;
 WiFiClient espClient;
-PubSubClient client("siwatsystem.com", 1883, espClient);
-PubSubClientTools mqtt(client);
 MySQL_Connection conn(&espClient);
-MySQL_Cursor* cursor;
+MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
 ThreadController threadControl = ThreadController();
 Thread mqttupdater = Thread();
 Thread datacollector = Thread();
@@ -48,6 +43,50 @@ FlowMeter sensor2 = FlowMeter(13);
 volatile boolean awakenByInterrupt = false;
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 int menu;
+float retrieveVOL(){
+  char query[] = "SELECT population FROM world.city WHERE name = 'New York'";
+  row_values *row = NULL;
+  long head_count = 0;
+
+  delay(1000);
+
+  Serial.println("1) Demonstrating using a cursor dynamically allocated.");
+  // Execute the query
+  cur_mem->execute(query);
+  // Fetch the columns (required) but we don't use them.
+  column_names *columns = cur_mem->get_columns();
+
+  // Read the row (we are only expecting the one)
+  do {
+    row = cur_mem->get_next_row();
+    if (row != NULL) {
+      head_count = atol(row->values[0]);
+    }
+  } while (row != NULL);
+  // Deleting the cursor also frees up memory used
+  delete cur_mem;
+
+  // Show the result
+  Serial.print("  NYC pop = ");
+  Serial.println(head_count);
+
+  delay(500);
+
+  Serial.println("2) Demonstrating using a local, global cursor.");
+  // Execute the query
+  cur.execute(query);
+  // Fetch the columns (required) but we don't use them.
+  cur.get_columns();
+  // Read the row (we are only expecting the one)
+  do {
+    row = cur.get_next_row();
+    if (row != NULL) {
+      head_count = atol(row->values[0]);
+    }
+  } while (row != NULL);
+  // Now we close the cursor to free any memory
+  cur.close();
+}
 void writelcd(String line1, String line2){
   lcd.clear();
   lcd.print(line1);
@@ -69,10 +108,7 @@ void collectdata() {
   sensor2.tick(1000);
 }
 void updatemqtt() {
-  mqtt.publish("/waterishos/node" + nodename + "/flowrate1", String(sensor1.getCurrentFlowrate()));
-  mqtt.publish("/waterishos/node" + nodename + "/volume1", String(sensor1.getTotalVolume()));
-  mqtt.publish("/waterishos/node" + nodename + "/flowrate2", String(sensor2.getCurrentFlowrate()));
-  mqtt.publish("/waterishos/node" + nodename + "/volume2", String(sensor2.getTotalVolume()));
+  //UPDATE USING MySQL
 }
 void setup() {
   lcd.init();
@@ -120,7 +156,7 @@ void setup() {
   writelcd("Waterish OS a3.9","Reading  Sensors");
   delay(1000);
   writelcd(" Telemetry Node","siwatsystem.com");
-  if (client.connect("waterishos",telemetryuser,telemetrykey)) {
+  if (conn.connect(server_addr, 3306, mysqluser, mysqlpassword)) {
     writelcd(" Telemetry Node","Connected");
     delay(1000);
   } else {
@@ -128,6 +164,8 @@ void setup() {
     online=false;
     delay(3000);
   }
+  //MySQL Persist Object Retrival
+  
   datacollector.onRun(collectdata);
   datacollector.setInterval(1000);
   if(online)mqttupdater.onRun(updatemqtt);
